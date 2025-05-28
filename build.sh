37fb154ff77490a2893cb5a6295f35b7cb5e4c95
yanhuo075 <134347809+yanhuo075@users.noreply.github.com>
2025-05-28 15:36:34 +0800

    Update build.sh
    Signed-off-by: yanhuo075 <134347809+yanhuo075@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -2,21 +2,16 @@
 
 # 配置环境
 npm install -g hexo-cli
-mkdir hexo
-cd hexo
 hexo init
 npm install
 rm -rf source/_posts
 mkdir -p source/_posts
 
 # 安装主题(你们可以改成自己喜欢的主题)
-git https://github.com/fluid-dev/hexo-theme-fluid themes/hexo-theme-fluid
+git clone https://github.com/fluid-dev/hexo-theme-fluid themes/hexo-theme-fluid
 
 # 安装主题
 npm install --save hexo-theme-fluid
-
-# 复制配置文件
-cp config.yml hexo
 
 # 开始构建
 cd hexo && hexo g --config config.yml
